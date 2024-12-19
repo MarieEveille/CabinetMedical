11147625f207589f4b907044bd930107c8c7eab2
MarieEveille <eveille.marie29@gmail.com>
2024-12-19 09:08:16 +0100

    [INIT] Services Praticien, Rendez-vous, Gateway, Eureka, Dossier Médical

---
+++ Dossier_Medical/src/main/java/org/example/dossier_medical/DossierMedicalApplication.java
@@ -0,0 +1,13 @@
+package org.example.dossier_medical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DossierMedicalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DossierMedicalApplication.class, args);
+    }
+
+}
---
+++ Praticien/src/test/java/org/example/praticien/PraticienApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.praticien;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PraticienApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
